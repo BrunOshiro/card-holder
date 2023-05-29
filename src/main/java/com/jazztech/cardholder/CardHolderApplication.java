95868e8d31ba55de3b4a2cc2694e4f479604c8c3
BrunOshiro <brunoshiro93@gmail.com>
2023-05-29 11:53:00 -0300

    Initial commit

---
+++ src/main/java/com/jazztech/cardholder/CardHolderApplication.java
@@ -0,0 +1,13 @@
+package com.jazztech.cardholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardHolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardHolderApplication.class, args);
+	}
+
+}
