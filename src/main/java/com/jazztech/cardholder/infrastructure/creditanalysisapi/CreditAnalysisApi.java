e01e9be94fc5ce3e5d11d33d8da54b4741a7b50d
BrunOshiro <brunoshiro93@gmail.com>
2023-06-20 12:14:43 -0300

    refactor - adjusted creditanalysis api url

--- src/main/java/com/jazztech/cardholder/infrastructure/creditanalysisapi/CreditAnalysisApi.java
+++ src/main/java/com/jazztech/cardholder/infrastructure/creditanalysisapi/CreditAnalysisApi.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-@FeignClient(name = "credit-analysis", url = "http://localhost:8082")
+@FeignClient(name = "credit-analysis", url = "http://localhost:8081")
 public interface CreditAnalysisApi {
-    @GetMapping("api/v.1.0.0/credit/analysis/")
+    @GetMapping("api/v1/credit/analysis/")
     CreditAnalysisDto getCreditAnalysisByCpfOrClientId(
             @RequestParam(value = "clientId", required = false) UUID clientId,
             @RequestParam(value = "cpf") @Validated String cpf
