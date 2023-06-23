fec6ad7d084d15dbe8d82e531a2876e992b16e0c
BrunOshiro <brunoshiro93@gmail.com>
2023-06-23 10:41:28 -0300

    style - fixed the checkstyle problem

--- src/main/java/com/jazztech/cardholder/presentation/dto/CardHolderRequestDto.java
+++ src/main/java/com/jazztech/cardholder/presentation/dto/CardHolderRequestDto.java
@@ -15,14 +15,21 @@ public record CardHolderRequestDto(
         BankAccount bankAccount
 ) {
     public boolean isBankAccountValid() {
-        return bankAccount == null || (bankAccount.account() != null && bankAccount.agency() != null && bankAccount.bankCode() != null);
+        final Boolean isBankAccountGroupFilled = bankAccount == null;
+        final Boolean isBankAccountAttributesFilled = bankAccount != null
+                && bankAccount.account() != null
+                && bankAccount.agency() != null
+                && bankAccount.bankCode() != null;
+        return isBankAccountGroupFilled || isBankAccountAttributesFilled;
     }
 
     @Builder(toBuilder = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public record BankAccount(
+
             @Size(min = 6, max = 10, message = "Account must be between 6 and 10 characters")
             String account,
+
             @Size(min = 4, max = 5, message = "Agency must be between 4 and 5 characters")
             String agency,
             @Size(min = 3, max = 3, message = "Bank Code must be 3 characters")
