cd89157be0f588b8281167ebfd96de3dd339b303
BrunOshiro <brunoshiro93@gmail.com>
2023-06-28 12:05:09 -0300

    feat - added builder to the CardResponseDto

--- src/main/java/com/jazztech/cardholder/presentation/dto/CardResponseDto.java
+++ src/main/java/com/jazztech/cardholder/presentation/dto/CardResponseDto.java
@@ -2,7 +2,9 @@ package com.jazztech.cardholder.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.UUID;
+import lombok.Builder;
 
+@Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record CardResponseDto(
         UUID cardId,
