d6c1815451659b6c4316928157594ea305b1e04f
BrunOshiro <brunoshiro93@gmail.com>
2023-06-28 07:08:31 -0300

    feat - create a new field into the ddl creditLimitAvailable and added the table card

--- data/ddl.sql
+++ data/ddl.sql
@@ -4,6 +4,7 @@ CREATE TABLE card_holder (
    credit_analysis_id UUID,
    status VARCHAR(255) CHECK(status IN ('ACTIVE','INACTIVE')),
    credit_limit NUMERIC(10,2),
+   credit_limit_available NUMERIC(10,2),
    bank_account_id UUID,
    created_at TIMESTAMP WITHOUT TIME ZONE,
    CONSTRAINT pk_card_holder PRIMARY KEY (id)
@@ -19,3 +20,16 @@ CREATE TABLE bank_account (
    created_at TIMESTAMP WITHOUT TIME ZONE,
    CONSTRAINT pk_bank_account PRIMARY KEY (id)
 );
+
+CREATE TABLE card (
+  id UUID NOT NULL,
+   card_holder_id UUID,
+   credit_limit NUMERIC(10,2),
+   card_number VARCHAR(20),
+   cvv VARCHAR(3),
+   due_date VARCHAR(20),
+   created_at TIMESTAMP WITHOUT TIME ZONE,
+   CONSTRAINT pk_card PRIMARY KEY (id)
+);
+
+ALTER TABLE card ADD CONSTRAINT FK_CARD_ON_CARD_HOLDER FOREIGN KEY (card_holder_id) REFERENCES card_holder (id);
